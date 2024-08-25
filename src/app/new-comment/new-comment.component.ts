0afb4de769e427bad2ce82d8ccc523bcdcc01f2e
kbiesbrock <kbiesbrock@gmail.com>
2024-08-24 22:45:55 -0400

    Initial @suggestions implementation -kb

--- src/app/new-comment/new-comment.component.ts
+++ src/app/new-comment/new-comment.component.ts
@@ -1,23 +1,185 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { HighlightPipe } from '../highlight/highlight.pipe';
+
+type User = {
+  userID: number;
+  name: string;
+};
 
 @Component({
   selector: 'new-comment',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, HighlightPipe],
   templateUrl: './new-comment.component.html',
-  styleUrl: './new-comment.component.scss',
 })
 export class NewCommentComponent {
   protected comment = new FormControl('');
+  @ViewChild('commentInput', { static: false })
+  protected commentInputElement!: HTMLInputElement;
+  protected taggedUserIDs: User[] = [];
+  protected searchString: string = '';
+  protected filteredSuggestions: User[] = [];
+  protected showSuggestions = false;
+  protected highlightedSuggestionIndex = -1;
 
-  @Output() addNewComment = new EventEmitter<String>();
+  private suggestions: User[] = [
+    { userID: 1, name: 'Kevin' },
+    { userID: 2, name: 'Jeff' },
+    { userID: 3, name: 'Bryan' },
+    { userID: 4, name: 'Gabbey' },
+  ];
+
+  @Output() addNewComment = new EventEmitter<{
+    comment: String;
+    taggedUsers: User[];
+  }>();
 
   emitComment(event: Event) {
     event.preventDefault();
     event.stopPropagation();
-    if (this.comment.value) {
-      this.addNewComment.emit(this.comment.value);
+    const value = this.comment.value || '';
+    if (value === null) {
+      return;
     }
+    this.addNewComment.emit({
+      comment: value,
+      taggedUsers: this.taggedUserIDs,
+    });
+    this.comment.setValue('');
+    this.resetSuggestions();
+    this.taggedUserIDs = [];
+  }
+
+  onKeyPress(event: KeyboardEvent) {
+    // console.log(event);
+
+    if (!this.showSuggestions) {
+      return;
+    }
+
+    switch (event.code) {
+      case 'Tab':
+        event.preventDefault();
+        if (event.shiftKey) {
+          this.decreaseSuggestionIndex();
+        } else {
+          this.increaseSuggestionIndex();
+        }
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        this.decreaseSuggestionIndex();
+        break;
+      case 'ArrowDown':
+        event.preventDefault();
+        this.decreaseSuggestionIndex();
+        break;
+      case 'Enter':
+        event.preventDefault();
+        this.suggestionSelected(
+          this.filteredSuggestions[this.highlightedSuggestionIndex],
+          this.commentInputElement
+        );
+        this.resetSuggestions();
+        break;
+      case 'Escape':
+        event.preventDefault();
+        this.resetSuggestions();
+    }
+  }
+
+  /**
+   * Conditions for when to show suggestions:
+   *  1. When "@" is first typed
+   *  2. After "@" is typed, suggestions closed, and backspace key is used within
+   *     within the same word
+   *  3. When user taps into input, begins typing where word is prefixed with "@"
+   * @param event The input event
+   * @returns void
+   */
+  onInputChange(event: Event) {
+    const inputElement: HTMLInputElement = event.target as HTMLInputElement;
+    const value: string | null = this.comment.value || null;
+    if (value === null || value === '') {
+      this.resetSuggestions();
+      return; // Nothing to filter here.
+    }
+
+    const limitLastIndexStartToCursorLocation =
+      inputElement.selectionStart || undefined;
+    const lastAtIndex = value.lastIndexOf(
+      '@',
+      limitLastIndexStartToCursorLocation
+    );
+
+    if (lastAtIndex === -1) {
+      this.resetSuggestions();
+      return;
+    }
+
+    this.showSuggestions = true;
+
+    const cursorPosition: number = inputElement.selectionStart || 0;
+    this.searchString = value.substring(lastAtIndex + 1, cursorPosition);
+
+    console.log(this.searchString, lastAtIndex, cursorPosition);
+
+    if (
+      this.searchString.includes(' ') ||
+      this.searchString.match(/[^a-zA-Z]/g) !== null
+    ) {
+      this.resetSuggestions();
+      return;
+    }
+
+    this.filteredSuggestions = this.suggestions.filter((suggestion) =>
+      suggestion.name.toLowerCase().includes(this.searchString.toLowerCase())
+    );
+  }
+
+  resetSuggestions() {
+    this.showSuggestions = false;
+    this.filteredSuggestions = [];
+    this.highlightedSuggestionIndex = -1;
+  }
+
+  decreaseSuggestionIndex() {
+    if (this.highlightedSuggestionIndex <= 0) {
+      this.highlightedSuggestionIndex = this.filteredSuggestions.length - 1;
+      return;
+    }
+    this.highlightedSuggestionIndex -= 1;
+  }
+
+  increaseSuggestionIndex() {
+    if (
+      this.highlightedSuggestionIndex >=
+      this.filteredSuggestions.length - 1
+    ) {
+      this.highlightedSuggestionIndex = 0;
+      return;
+    }
+    this.highlightedSuggestionIndex += 1;
+  }
+
+  suggestionSelected(selectedUser: User, inputElement: HTMLInputElement) {
+    const comment = this.comment.value || '';
+    const narrowLastIndexSearchToCursorLocation = inputElement.selectionStart;
+    const lastAtIndex = comment.lastIndexOf(
+      '@',
+      narrowLastIndexSearchToCursorLocation || comment.length
+    );
+    let updatedComment = comment.substring(0, lastAtIndex);
+    updatedComment += '@' + selectedUser.name;
+    updatedComment += comment.substring(
+      lastAtIndex + 1 + this.searchString.length
+    );
+
+    console.log(comment, this.searchString, selectedUser.name, updatedComment);
+    this.comment.setValue(updatedComment);
+    this.taggedUserIDs.includes(selectedUser) ||
+      this.taggedUserIDs.push(selectedUser);
+    this.resetSuggestions();
   }
 }
